8e60b54df1c1eddc004d95688fc1009b5e6e89c8
Najat <craazylolla@gmail.com>
2023-12-20 12:31:48 -0800

    m

--- models/user.py
+++ models/user.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """This is the user class"""
+from models.base_model import BaseModel
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
 
 
+class User(BaseModel):
 class User(BaseModel, Base):
     """This is the class for user
     Attributes:
@@ -16,8 +15,12 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
     """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
