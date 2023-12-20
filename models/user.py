7b054c8f6a129f207a127c98b2dce04494a973f1
Najat <craazylolla@gmail.com>
2023-12-20 12:59:32 -0800

    m

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -23,3 +24,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
