fe7ee19906acdff38026e15f05535e7f6848a956
Najat <craazylolla@gmail.com>
2024-01-28 21:22:37 +0000

    m

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Start a Flask web application"""
+
+from flask import Flask
+from werkzeug.utils import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display 'Hello HBNB!' on the root route."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display 'HBNB' on the /hbnb route."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Display 'C ' followed by the value of the text variable."""
+    return 'C {}'.format(escape(text.replace('_', ' ')))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python_route(text='is cool'):
+    """Display 'Python ' followed by the value of the text variable."""
+    return 'Python {}'.format(escape(text.replace('_', ' ')))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
